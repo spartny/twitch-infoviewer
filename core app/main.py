419868b3922f190da6b5c8fd71b7d91d235a3d66
Arnav Ignatius <105193713+spartny@users.noreply.github.com>
2023-05-07 15:13:10 +0530

    path change

--- core app/main.py
+++ core app/main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     
     app.setApplicationName("Twitch InfoViewer")
-    ui_file_name = "twitch-infoviewer/main.ui"
+    ui_file_name = "twitch-infoviewer/core app/main.ui"
     ui_file = QFile(ui_file_name)
     if not ui_file.open(QIODevice.ReadOnly):
         print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
