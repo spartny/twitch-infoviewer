76313a3e01c1ef18c6a6f7cfdf00dbd62006b138
daxamengineer <anishkhemani@gmail.com>
2023-05-10 16:23:28 +0530

    requests update

--- core app/APIData.py
+++ core app/APIData.py
@@ -1,64 +1,85 @@
-import os
-import subprocess
+import requests
 
+headers = {"Client-ID": "aa00f8e0g4gic1bkruejtn1qvht9v3","Authorization": "Bearer fd253rukwe5shngahs4eb1bzxlz7e8"}
+url = "https://api.twitch.tv/helix/"
 
-client_id = 'aa00f8e0g4gic1bkruejtn1qvht9v3'
-client_secret = 'scpcq6f9io95x79pw1hhhb3yjiyxnu'
-config_cmd = "twitch configure -i " + client_id + " -s " + client_secret
-os.system('cmd /C ' + config_cmd)
-
-
-username = input("Enter username: ")
-id_cmd = "twitch api get /users -q login=" + username
-data_dict = eval(subprocess.check_output(id_cmd,shell=True))
-ID = data_dict['data'][0]['id']
+def getid(username):
+    user_url = url + "users?login=" + username
+    response = requests.get(user_url, headers=headers).json()
+    return response['data'][0]['id']
 
 #API DATA
 
-def getUser(): #prints data of specific user
-    user_cmd = "twitch api get /users -q login=" + username
-    os.system('cmd /C ' + user_cmd)
+def getUserDetails(username): #prints data of specific user
+    user_url = url + "users?login=" + username
+    response = requests.get(user_url, headers=headers).json()
+    print(response["data"][0])
 
-def getTopGames(): #prints data of top 20 broadcasted games
-    topgames_cmd = "twitch api get /games/top"
-    os.system('cmd /C ' + topgames_cmd)
+def getTop20Games(): #prints data of top 20 broadcasted games
+    topgames_url = url + "games/top" 
+    response = requests.get(topgames_url, headers=headers).json()
+    for game in response["data"]:
+        print(game)
+        print()
 
-def getGame(): #prints data of specific game
-    game = input("Enter game name: ")
-    game_cmd = "twitch api get /games -q name=" + game
-    os.system('cmd /C ' + game_cmd)
+def getGameDetails(game): #prints data of specific game
+    game_url = url + "games?name=" + game
+    response = requests.get(game_url, headers=headers).json()
+    print(response["data"][0])
 
 def getGlobalEmotes(): #prints data of all global emotes that are used in chat
-    emote_cmd = "twitch api get /chat/emotes/global"
-    os.system('cmd /C ' + emote_cmd)
+    emote_url = url + "chat/emotes/global"
+    response = requests.get(emote_url, headers=headers).json()
+    for emote in response["data"]:
+        print(emote)
+        print()
 
-def getStreams(): #prints all current streams
-    stream_cmd = "twitch api get /streams"
-    os.system('cmd /C ' + stream_cmd)
+def getTop20Streams(): #prints top 20 current streams
+    stream_url = url + "streams"
+    response = requests.get(stream_url, headers=headers).json()
+    for stream in response["data"]:
+        print(stream)
+        print()
 
-def getSoundtrackPlaylists(): #prints Soundtrack Playlists
-    music_cmd = "twitch api get /soundtrack/playlists"
-    os.system('cmd /C ' + music_cmd)
+def getTop20SoundtrackPlaylists(): #prints top 20 Soundtrack Playlists
+    music_url = url + "soundtrack/playlists"
+    response = requests.get(music_url, headers=headers).json()
+    for playlist in response["data"]:
+        print(playlist)
+        print()
 
-def getChannelFollowers(ID): 
-    channel_cmd = 'twitch api get /channels/followers -q broadcaster_id='+ID
-    os.system("cmd /C" + channel_cmd)
+def getFollowerCount(channel):  #prints number of followers of a specific account
+    channel_url = url + "channels/followers?broadcaster_id=" + getid(channel)
+    response = requests.get(channel_url, headers=headers).json()
+    print(response["total"])
+    
+def getUserFollows(username):  #prints details of all followed accounts of a specific user
+    follows_url = url + "users/follows?from_id="+ getid(username)
+    response = requests.get(follows_url, headers=headers).json()
+    for user in response["data"]:
+        print(user)
+        print()
 
-def getUsersFollows(ID):
-    follows_cmd = 'twitch api get /users/follows -q from_id='+ID
-    os.system("cmd /C" + follows_cmd)
+def getFollowers(username):  #prints details of all followers of a specific user
+    follower_url = url + "users/follows?to_id="+ getid(username)
+    response = requests.get(follower_url, headers=headers).json()
+    for user in response["data"]:
+        print(user)
+        print()
 
-def getUsersFollowing(ID):
-    following_cmd = 'twitch api get /users/follows -q to_id='+ID
-    os.system("cmd /C" + following_cmd)
+def getVideoClipDetails(username):  #prints details of video clips of a specific user
+    video_url = url + "clips?broadcaster_id="+ getid(username)
+    response = requests.get(video_url, headers=headers).json()
+    for clip in response["data"]:
+        print(clip)
+        print()
 
-def getVideoClip(ID):
-    clip_cmd = 'twitch api get /clips -q broadcaster_id='+ID
-    os.system("cmd /C" + clip_cmd)
+def getChannelInfo(channel):  #prints channel data
+    info_url = url + "channels?broadcaster_id="+ getid(channel)
+    response = requests.get(info_url, headers=headers).json()
+    print(response['data'][0])
 
-def getChannelInfo(ID):
-    Info_cmd = 'twitch api get /channels -q broadcaster_id='+ID
-    os.system("cmd /C" + Info_cmd)
+
 
 
 
