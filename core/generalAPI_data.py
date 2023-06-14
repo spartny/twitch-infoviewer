4d27414fb477a488c081d4097db5f703244cdd3a
daxamengineer <anishkhemani@gmail.com>
2023-06-14 22:13:14 +0530

    additional APIs 2

--- core/generalAPI_data.py
+++ core/generalAPI_data.py
@@ -10,11 +10,13 @@ def extraction(choice, username):
         "Global Emotes": getGlobalEmotes,
         "Top 20 Streams": getTop20Streams,
         "Top 20 Soundtrack Playlists": getTop20SoundtrackPlaylists,
+        "Global Chat Badges": getGlobalChatBadges,
+        "Global Cheermotes": getGlobalCheermotes
     }
     return options[choice](username)
 
 
-def getTop20Games(username):  # prints data of top 20 broadcasted games
+def getTop20Games():  # prints data of top 20 broadcasted games
     topgames_url = url + "games/top"
     response = requests.get(topgames_url, headers=headers).json()
     data = response["data"]
@@ -24,7 +26,7 @@ def getTop20Games(username):  # prints data of top 20 broadcasted games
     return result
 
 
-def getGlobalEmotes(username):  # prints data of all global emotes that are used in chat
+def getGlobalEmotes():  # prints data of all global emotes that are used in chat
     emote_url = url + "chat/emotes/global"
     response = requests.get(emote_url, headers=headers).json()
     data = response["data"]
@@ -34,7 +36,7 @@ def getGlobalEmotes(username):  # prints data of all global emotes that are used
     return result
 
 
-def getTop20Streams(username):  # prints top 20 current streams
+def getTop20Streams():  # prints top 20 current streams
     stream_url = url + "streams"
     response = requests.get(stream_url, headers=headers).json()
     data = response["data"]
@@ -44,12 +46,32 @@ def getTop20Streams(username):  # prints top 20 current streams
     return result
 
 
-def getTop20SoundtrackPlaylists(username):  # prints top 20 Soundtrack Playlists
-    music_url = url + "soundtrack/playlists"
+def getTop20SoundtrackPlaylists():  # prints top 20 Soundtrack Playlists
+    music_url = url + "badges/global"
     response = requests.get(music_url, headers=headers).json()
     data = response["data"]
     result = ""
     for i in data:
         result += str(i) + '\n'
     return result
+
+def getGlobalChatBadges():  # prints all global chat badges
+    badge_url = url + "soundtrack/playlists"
+    response = requests.get(badge_url, headers=headers).json()
+    data = response["data"]
+    result = ""
+    for i in data:
+        result += str(i) + '\n'
+    return result
+
+def getGlobalCheermotes():  # prints all global chat badges
+    cheer_url = url + "bits/cheermotes"
+    response = requests.get(cheer_url, headers=headers).json()
+    data = response["data"][0]["tiers"]
+    result = ""
+    for i in data:
+        result += str(i) + '\n'
+    return result
+
+
 
